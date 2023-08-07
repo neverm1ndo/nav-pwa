458f44a3d21103345a58ad81603284f0c98ffc7d
neverm1ndo <it@nmnd.ru>
2023-08-07 23:17:28 +0300

    initial commit

---
+++ vite.config.ts
@@ -0,0 +1,47 @@
+import { defineConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa';
+
+export default defineConfig({
+  plugins: [
+    VitePWA({ 
+      registerType: 'autoUpdate',
+      workbox: {
+        globPatterns: ['**/*'],
+      },
+      includeAssets: ['**/**'],
+      devOptions: {
+        enabled: true
+        /* other options */
+      },
+      manifest: {
+        name: 'PWA Nav',
+        short_name: 'PWANav',
+        display: 'standalone',
+        description: 'PWA Navigation app',
+        theme_color: '#242424',
+        icons: [
+          {
+            src: 'maskable_icon_x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'maskable_icon_x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          },
+          {
+            src: 'maskable_icon_x384.png',
+            sizes: '384x384',
+            type: 'image/png'
+          },
+          {
+            src: 'maskable_icon_x128.png',
+            sizes: '128x128',
+            type: 'image/png',
+          },
+        ]
+      }
+    })
+  ],
+})
