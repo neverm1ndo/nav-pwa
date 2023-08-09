d5e90bb64edf614bc257f87285867c92195fd1f4
neverm1ndo <it@nmnd.ru>
2023-08-09 17:57:52 +0300

    arch setup

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
       },
       includeAssets: ['**/**'],
       devOptions: {
-        enabled: true
+        // enabled: true
         /* other options */
       },
       manifest: {
